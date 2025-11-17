b80e4a53dba6083915cccc670b6c2f95c53ebc47
Andy Ansong <andyansong82@gmail.com>
2025-11-17 01:47:34 +0000

    create balance container

--- lib/features/main/presentation/homepage.dart
+++ lib/features/main/presentation/homepage.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  bool _isMoneyVisible = true;
+  int currentMoney = 500000;
+  void setIsMoneyVisible(){
+    setState(() {
+      _isMoneyVisible = !_isMoneyVisible;
+    });
+  }
+  String displayCurrentMoney(){
+    if(_isMoneyVisible){
+      final formatter = NumberFormat.currency(locale: 'en_GH', symbol: 'GHS ');
+      return formatter.format(currentMoney/100);
+    }else{
+      final hiddenMoney = "GHS ".padRight(8, "*");
+      return hiddenMoney;
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: 15,
       children: [
         // Salutations
         Row(
@@ -46,21 +71,66 @@ class Homepage extends StatelessWidget {
         ),
         
         // Check balance
-        // Container(
-        //   child: Column(
-        //     children: [
-        //       Text("Your balance"),
-        //       Row(
-        //         children: [
-        //           Text("Ghc 5,000.00"),
-        //           Icon(Icons.remove_red_eye)
-        //         ],
-        //       )
-        //     ],
-        //   )
-        // ),
+        Container(
+          padding: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.tertiary,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Your balance",
+                style: TextStyle(
+                  fontSize: Theme.of(context).textTheme.bodySmall?.fontSize,
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      displayCurrentMoney(),
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: setIsMoneyVisible,
+                      icon: Icon(
+                        _isMoneyVisible ? Icons.visibility_outlined : Icons.visibility_off_outlined
+                      )
+                    )
+                  ],
+                ),
+              ),
+              TextButton(
+                onPressed: (){},
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.inversePrimary)
+                ),
+                child: Text(
+                  "Add money",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.tertiary,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          )
+        ),
+
+        // Your cards
         // Container(),
-        // Container(),
+
+        // Transactions
         // Container(),
       ],
     );
