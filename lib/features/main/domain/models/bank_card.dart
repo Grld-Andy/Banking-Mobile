da587bc3bd8403b3fd36e17aac09d83863e4fe45
Andy Ansong <andyansong82@gmail.com>
2025-11-17 02:36:12 +0000

    add bank cards

---
+++ lib/features/main/domain/models/bank_card.dart
@@ -0,0 +1,13 @@
+class BankCard {
+  final String cardType;
+  final String cardNumber;
+  final String backgroundImage;
+  final String bankLogo;
+
+  BankCard({
+    required this.cardType,
+    required this.cardNumber,
+    required this.backgroundImage,
+    required this.bankLogo,
+  });
+}
--- lib/features/main/presentation/homepage.dart
+++ lib/features/main/presentation/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:mobile_bank/features/main/domain/models/bank_card.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -25,6 +26,28 @@ class _HomepageState extends State<Homepage> {
       return hiddenMoney;
     }
   }
+
+  final List<BankCard> myCards = [
+    BankCard(
+      cardType: "Debit Card",
+      cardNumber: ".... 1234",
+      backgroundImage: 'assets/images/mastercardBg.jpg',
+      bankLogo: 'assets/images/mastercard.png',
+    ),
+    BankCard(
+      cardType: "Credit Card",
+      cardNumber: ".... 5678",
+      backgroundImage: 'assets/images/visaBg.jpg',
+      bankLogo: 'assets/images/visa.png',
+    ),
+    BankCard(
+      cardType: "Bank",
+      cardNumber: ".... 5678",
+      backgroundImage: 'assets/images/bankBg.webp',
+      bankLogo: 'assets/images/bank.jpg',
+    ),
+  ];
+
 
 
   @override
@@ -128,7 +151,119 @@ class _HomepageState extends State<Homepage> {
         ),
 
         // Your cards
-        // Container(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Your cards",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                fontWeight: FontWeight.bold,
+                fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize
+              ),
+            ),
+            Text(
+              "+ New card",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                fontWeight: FontWeight.bold,
+                fontSize: Theme.of(context).textTheme.bodySmall?.fontSize
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 160,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: myCards.length,
+            separatorBuilder: (_, __) => SizedBox(width: 10),
+            itemBuilder: (context, index) {
+              final card = myCards[index];
+              return SizedBox(
+                width: 250,
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: Image.asset(
+                          card.backgroundImage,
+                          height: 160,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            card.bankLogo,
+                            height: 30,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                    card.cardType,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: Theme.of(context).textTheme.bodySmall?.fontSize
+                                    ),
+                                  ),
+                                  Text(
+                                    card.cardNumber,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              TextButton.icon(
+                                onPressed: (){},
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Theme.of(context).colorScheme.tertiary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10), // smaller radius
+                                  ),
+                                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15)
+                                ),
+                                icon: Icon(
+                                  Icons.visibility_outlined,
+                                  color: Theme.of(context).colorScheme.inversePrimary
+                                ),
+                                label: Text(
+                                  "Details",
+                                  style: TextStyle(
+                                    color: Theme.of(context).colorScheme.inversePrimary,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: Theme.of(context).textTheme.bodyMedium?.fontSize
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ]
+                ),
+              );
+            }
+          ),
+        ),
 
         // Transactions
         // Container(),
